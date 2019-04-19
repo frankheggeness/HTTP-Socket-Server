6ada100913641558d437ab70a6ad18899643eb08
Frank <fr.heggeness@gmail.com>
2019-04-18 16:58:50 -1000

    client

--- client.js
+++ client.js
@@ -1,3 +1,56 @@
 'use strict';
 
 const net = require('net');
+let port = 80;
+
+let args = process.argv;
+let date = new Date();
+
+if (!args[2]) {
+  process.stdout.write('no argument, try request /index.html');
+  process.exit();
+}
+
+let argument = args[2];
+let findHTML = argument.indexOf('.com');
+let findHost = argument.substring(0, findHTML + 4);
+let newRequest = argument.substring(findHTML + 4, argument.length);
+
+if (argument.indexOf('localhost') !== -1) {
+  findHost = 'localhost';
+  port = 8080;
+
+  let findIndex = argument.indexOf('/');
+
+  newRequest = argument.substring(findIndex, argument.length);
+}
+
+let request = `GET ${newRequest} HTTP/1.1\r\n`;
+request += `host: ${findHost}\r\n`;
+request += `date: ${new Date().toUTCString()}\r\n`;
+request += `\r\n`;
+
+console.log(request);
+
+const client = net.createConnection(port, findHost, () => {
+  client.setEncoding('utf-8');
+
+  client.on('connect', () => {
+    console.log('connected');
+  });
+
+  client.write(request);
+});
+
+client.on('data', (data) => {
+  console.log(data.toString());
+});
+
+client.on('end', () => {
+  console.log('The connection has ended');
+});
+
+client.on('error', () => {
+  console.log('CAUTION ERROR');
+  process.exit();
+});
